b2d8a1262e5acb6ba478277acc334c28a2175c87
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2023-11-17 15:16:35 -0500

    Initial Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
+import { SupermarketsModule } from './supermarkets/supermarkets.module';
 
 @Module({
-  imports: [],
+  imports: [CitiesModule, SupermarketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
